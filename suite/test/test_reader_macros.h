a9ef3ce178b5f1d14f09d4800f42f5b2ddb4212d
wiluite <tetasoft@yandex.ru>
2024-12-06 16:19:31 +0300

    Remove unnecessary csv reader definitions for tests.

--- suite/test/test_reader_macros.h
+++ suite/test/test_reader_macros.h
@@ -5,14 +5,14 @@
 
 #pragma once
 
-#define TEST_TRIM_POLICY_READER(NAME)                                             \
-    struct NAME {                                                                 \
+#define TEST_TRIM_POLICY_READER(POLICY_NAME)                                      \
+    struct POLICY_NAME {                                                          \
     public:                                                                       \
         inline static void trim(std::string &) {}                                 \
         inline static auto ret_trim(std::string s) { return s; }                  \
         inline static auto ret_trim(csv_co::unquoted_cell_string s) { return s; } \
     };                                                                            \
-    using test_reader_##NAME = csv_co::reader<NAME>;
+    using test_reader_##POLICY_NAME = csv_co::reader<POLICY_NAME>;
 
     TEST_TRIM_POLICY_READER(r1)   // test_reader_r1
     TEST_TRIM_POLICY_READER(r2)   // test_reader_r2
@@ -20,12 +20,3 @@
     TEST_TRIM_POLICY_READER(r4)   // test_reader_r4
     TEST_TRIM_POLICY_READER(r5)   // test_reader_r5
     TEST_TRIM_POLICY_READER(r6)   // test_reader_r6
-    TEST_TRIM_POLICY_READER(r7)   // test_reader_r7
-    TEST_TRIM_POLICY_READER(r8)   // test_reader_r8
-    TEST_TRIM_POLICY_READER(r9)   // test_reader_r9
-    TEST_TRIM_POLICY_READER(r10)  // test_reader_r10
-    TEST_TRIM_POLICY_READER(r11)  // test_reader_r11
-    TEST_TRIM_POLICY_READER(r12)  // test_reader_r12
-    TEST_TRIM_POLICY_READER(r13)  // test_reader_r13
-    TEST_TRIM_POLICY_READER(r14)  // test_reader_r14
-    TEST_TRIM_POLICY_READER(r15)  // test_reader_r15
