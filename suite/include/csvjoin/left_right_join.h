be2f2b1135e3d2665be4f4d8bfee2819bef17bc5
wiluite <tetasoft@yandex.ru>
2024-12-20 20:02:42 +0300

    Left and right joins speed-up in action!

--- suite/include/csvjoin/left_right_join.h
+++ suite/include/csvjoin/left_right_join.h
@@ -7,9 +7,7 @@ auto left_or_right_join = [&deq, &ts_n_blanks, &c_ids, &args, &cycle_cleanup, &c
         auto const & [types1, blanks1] = ts_n_blanks[1];
 #else
         auto const & types0 = std::get<0>(ts_n_blanks[0]);
-        auto const & blanks0 = std::get<1>(ts_n_blanks[0]);
         auto const & types1 = std::get<0>(ts_n_blanks[1]);
-        auto const & blanks1 = std::get<1>(ts_n_blanks[1]);
 #endif
         reader_fake<reader_type> impl{0, 0};
 
@@ -25,43 +23,48 @@ auto left_or_right_join = [&deq, &ts_n_blanks, &c_ids, &args, &cycle_cleanup, &c
         if (can_compare(types0, types1)) {
             using namespace ::csvsuite::cli::compare;
             using elem_t = typename std::decay_t<decltype(std::get<0>(deq.front()))>::template typed_span<quoted>;
-#if !defined(__clang__) || __clang_major__ >= 16
-            auto [_, fun] = obtain_compare_functionality<elem_t>(blanks0[c_ids[0]] >= blanks1[c_ids[1]] ? std::vector<unsigned>{c_ids[0]} : std::vector<unsigned>{c_ids[1]}
-            , blanks0[c_ids[0]] >= blanks1[c_ids[1]] ? std::tuple{types0, blanks0} : std::tuple{types1, blanks1}, args)[0];
-#else
-            auto fun = std::get<1>(obtain_compare_functionality<elem_t>(blanks0[c_ids[0]] >= blanks1[c_ids[1]] ? std::vector<unsigned>{c_ids[0]} : std::vector<unsigned>{c_ids[1]}
-            , blanks0[c_ids[0]] >= blanks1[c_ids[1]] ? std::tuple{types0, blanks0} : std::tuple{types1, blanks1}, args)[0]);
-#endif
+
             auto & this_source = deq.front();
             auto & other_source = deq[1];
 
             std::visit([&](auto &&arg) {
 
+                assert(!std::holds_alternative<reader_fake<reader_type>>(other_source));
+
+                auto & other_reader = std::get<0>(other_source);
+
+                compromise_table_MxN other(other_reader, args);
+                auto compare_fun = obtain_compare_functionality<elem_t>(c_ids[1], ts_n_blanks[1], args);
+                std::stable_sort(poolstl::par, other.begin(), other.end(), sort_comparator(compare_fun, std::less<>()));
+
+                auto cache_types = [&] {
+                    for_each(poolstl::par, other.begin(), other.end(), [&](auto &item) {
+                        for (auto & elem : item) {
+                            using UElemType = typename std::decay_t<decltype(elem)>::template rebind<csv_co::unquoted>::other;
+                            elem.operator UElemType const&().type();
+                        }
+                    });
+                };
+
+                cache_types();
+
                 arg.run_rows([&](auto &span) {
+                    bool were_joins = false;
+                    auto key = elem_t{span[c_ids[0]]};
+                    const auto p = std::equal_range(other.begin(), other.end(), key, equal_range_comparator<reader_type>(compare_fun));
+                    for (auto next = p.first; next != p.second; ++next) {
+                        std::vector<std::string> join_vec;
 
-                    std::visit([&](auto &&arg) {
-
-                        bool were_joins = false;
-
-                        arg.run_rows([&](auto &span1) {
-
-                            std::visit([&](auto &&cmp) {
-                                if (!cmp(elem_t{span[c_ids[0]]}, elem_t{span1[c_ids[1]]})) {
-                                    std::vector<std::string> join_vec;
-                                    assert(types1.size() == span1.size());
-                                    join_vec.reserve(span.size() + span1.size() - 1);
-                                    join_vec.assign(span.begin(), span.end());
-                                    join_vec.insert(join_vec.end(), span1.begin(), span1.begin() + c_ids[1]);
-                                    join_vec.insert(join_vec.end(), span1.begin() + c_ids[1] + 1, span1.end());
-                                    impl.add(std::move(join_vec));
-                                    if (!were_joins)
-                                        were_joins = true;
-                                }
-                            }, fun);
-                        });
+                        join_vec.reserve(span.size() + next->size() - 1);
+                        join_vec.assign(span.begin(), span.end());
+                        join_vec.insert(join_vec.end(), next->begin(), next->begin() + c_ids[1]);
+                        join_vec.insert(join_vec.end(), next->begin() + c_ids[1] + 1, next->end());
+                        impl.add(std::move(join_vec));
                         if (!were_joins)
-                            impl.add(std::move(compose_distinct_record(span)));
-                    }, other_source);
+                            were_joins = true;
+                    }
+                    if (!were_joins)
+                        impl.add(std::move(compose_distinct_record(span)));
                 });
             }, this_source);
         } else {
