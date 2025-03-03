1d98ff462244a3a4bc5852fe75623006d4b0ff88
wiluite <tetasoft@yandex.ru>
2025-03-03 21:17:29 +0300

    compose_compare_function DRY principle applied.

--- suite/include/csvjoin/compare_composers.h
+++ suite/include/csvjoin/compare_composers.h
@@ -4,20 +4,20 @@ auto worst_of_blanks = [&] {
     return std::max(std::get<1>(ts_n_blanks[0])[c_ids[0]], std::get<1>(ts_n_blanks[1])[c_ids[1]]);
 };
 
-auto compose_compare_function = [&] {
-    auto blanks1_copy = std::get<1>(ts_n_blanks[1]);
+auto compose_compare_function_impl = [&] (auto & ts_n_blanks, auto & c_idx) {
+    auto blanks_copy = std::get<1>(ts_n_blanks);
 
-    blanks1_copy[c_ids[1]] = worst_of_blanks();
+    blanks_copy[c_idx] = worst_of_blanks();
     using elem_t = typename reader_type::template typed_span<quoted>;
-    return obtain_compare_functionality<elem_t>(c_ids[1], std::tuple{std::get<0>(ts_n_blanks[1]), blanks1_copy}, args);
+    return obtain_compare_functionality<elem_t>(c_idx, std::tuple{std::get<0>(ts_n_blanks), blanks_copy}, args);
+};
+
+auto compose_compare_function = [&] {
+    return compose_compare_function_impl(ts_n_blanks[1], c_ids[1]);
 };
 
 auto compose_symmetric_compare_function = [&] {
-    auto blanks0_copy = std::get<1>(ts_n_blanks[0]);
-
-    blanks0_copy[c_ids[0]] = worst_of_blanks();
-    using elem_t = typename reader_type::template typed_span<quoted>;
-    return obtain_compare_functionality<elem_t>(c_ids[0], std::tuple{std::get<0>(ts_n_blanks[0]), blanks0_copy}, args);
+    return compose_compare_function_impl(ts_n_blanks[0], c_ids[0]);
 };
 
 //------------------- This is just a code to inline it "in place" by the C preprocessor directive #include. See csvJoin.cpp --------------
